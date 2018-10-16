b56d025bdfe02f7500ac66af68eb5ab35bca9fe7
Hu Chao Xiang <charles@HumatoMacBook-Pro.local>
2018-10-15 20:03:21 -0400

    stuff

---
+++ clicky-game/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import cards from "./components/cards";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <cards/>
+    );
+  }
+}
+
+export default App;
