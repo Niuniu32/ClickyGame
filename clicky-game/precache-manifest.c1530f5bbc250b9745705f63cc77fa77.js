8ee03258c34d36141412bfabd50aadb84f97576b
Hu Chao Xiang <charles@HumatoMacBook-Pro.local>
2018-11-02 21:41:44 -0400

    react page

---
+++ clicky-game/precache-manifest.c1530f5bbc250b9745705f63cc77fa77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8abe40b2c1fc2b02ef2b7def51b43f7e",
+    "url": "/ClickyGame.git/static/media/bg-5-full.8abe40b2.jpg"
+  },
+  {
+    "revision": "32e446243a04662c37fa",
+    "url": "/ClickyGame.git/static/js/runtime~main.32e44624.js"
+  },
+  {
+    "revision": "b08e8ea5403416587b53",
+    "url": "/ClickyGame.git/static/js/main.b08e8ea5.chunk.js"
+  },
+  {
+    "revision": "378b5440c897e0ae8469",
+    "url": "/ClickyGame.git/static/js/1.378b5440.chunk.js"
+  },
+  {
+    "revision": "b08e8ea5403416587b53",
+    "url": "/ClickyGame.git/static/css/main.a0b42027.chunk.css"
+  },
+  {
+    "revision": "dd354367a3ec2a0edac085fa9232e2c1",
+    "url": "/ClickyGame.git/index.html"
+  }
+];
